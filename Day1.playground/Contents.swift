760dd9c860ad353a66756fb17cd7635550025b6b
Felarmir <felarmir@gmail.com>
2019-03-13 13:57:46 +0300

    Day 1

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+var str = "Hello, playground" // string
+str = "Goodbye"
+
+var ageInt = 31 // Int
+var population = 8_000_000 // Int
+
+// multi-line strings
+var str1 = """
+first line
+second line
+so it's good
+"""
+
+// one line and withoutn new line symbol
+var str2 = """
+first line \
+second line \
+so it's good
+"""
+
+// double
+var pi = 3.1415
+var g = 0.0000000000611
+
+
+// boolean
+var awesome = true
+var disgust = false
+
+// String interpolation
+var age = 31
+var ageStr = "age is equal \(age)"
+var ageMessage = "your \(ageStr)"
+
+
+// constants
+let taylor = "swift" // it's funny but constatnt declarete by let
+
+// type annotation
+
+let a_age: Int = 31
+let album: String = "Ocean"
+let year: Int = 1978
+let height: Double = 1.78
+let isRock: Bool = true
+
+//
