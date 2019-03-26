18a964a483c386fa920e4bedc8cf3392d86a6877
Felarmir <felarmir@gmail.com>
2019-03-26 09:59:39 +0300

    add Project 7

---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Denis Andreev on 3/25/19.
+//  Copyright © 2019 felarmir. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results:[Petition]
+}
